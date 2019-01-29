10e013c06378f2024237c38cf3eb7e5d1f8d775b
shinoby85 <ponk@tut.by>
2019-01-29 21:20:10 +0300

    Подготовлен шаблон для занятия 3.

---
+++ Lesson-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Понкратьев Евгений
+             * Задание 1: а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. 
+             * Продемонстрировать работу структуры.
+             * б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+             * в) Добавить диалог с использованием switch демонстрирующий работу класса.*/
+
+
+
+
+
+        }
+    }
+}
