d40fcd89da07187e0e194c4f47b7b47123709aec
shinoby85 <ponk@tut.by>
2019-02-13 01:02:43 +0300

    Выполнено з.2.п.1

--- Lesson-6/Program.cs
+++ Lesson-6/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Lesson_6
 {
     delegate double MathFunction(double data1, double data2);
@@ -57,6 +58,7 @@ namespace Lesson_6
             * Задание 1. Изменить программу вывода функции так, чтобы можно было передавать 
             * функции типа double (double,double). Продемонстрировать работу на функции 
             * с функцией a*x^2 и функцией a*sin(x).*/
+            /*
             Console.Write("Введите значение X: ");
             double x =Convert.ToDouble(Console.ReadLine());
             Console.Write("Введите значение переменной A: ");
@@ -65,8 +67,8 @@ namespace Lesson_6
 
             myTable(myFanc1, x, a);
             Console.WriteLine("-------------- Результат вычисления функции a*sin(x) --------------");
-            myTable(myFanc1, x, a);
-
+            myTable(myFanc2, x, a);
+            */
             /*Задание 2. Модифицировать программу нахождения минимума функции так, 
              * чтобы можно было передавать функцию в виде делегата.
              * а) Сделайте меню с различными функциями и предоставьте пользователю выбор, 
@@ -74,6 +76,58 @@ namespace Lesson_6
              * б) Используйте массив (или список) делегатов, в котором хранятся различные функции.
              * в) *Переделайте функцию Load, чтобы она возвращала массив считанных значений. 
              * Пусть она возвращает минимум через параметр.*/
+            double ax, bx, h;
+            
+            Console.Write("Введите промежуток, на котором произойдет выборка значсений: ");
+            Console.Write("AX: ");
+            ax=Convert.ToDouble(Console.ReadLine());
+            Console.Write("BX: ");
+            bx = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите шаг H: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выберите функцию для анализа.");
+            int reg = -1;
+            do
+            {
+                Console.WriteLine(@"1. Расчет по функции x * x - 50 * x + 10");
+                Console.WriteLine(@"2. Расчет по функции (2 * x + 1) / (3 * x - 1)");
+                Console.WriteLine(@"3. Расчет по функции x * x - 50 * x + 10");
+                Console.Write("Сделайте выбор: ");
+                reg = int.Parse(Console.ReadLine());
+            } while (reg<=0&&reg>=4);
+            switch (reg)
+            {
+                case 1:
+                    DoubleBinary.SaveFunc(DoubleBinary.F, ax, bx, h);
+                    Console.WriteLine($"Минимальное значение: {DoubleBinary.Load()}");
+                    break;
+                case 2:
+                    DoubleBinary.SaveFunc(DoubleBinary.F1, ax, bx, h);
+                    Console.WriteLine($"Минимальное значение: {DoubleBinary.Load()}");
+                    break;
+                case 3:
+                    DoubleBinary.SaveFunc(DoubleBinary.F2, ax, bx, h);
+                    Console.WriteLine($"Минимальное значение: {DoubleBinary.Load()}");
+                    break;
+                case 4:
+
+                    break;
+                
+            }
+
+
+
+
+
+
+            //SaveFunc("data.bin", -100, 100, 0.5);
+            //Console.WriteLine(Load("data.bin"));
+            //Console.ReadKey();
+
+
+
+
+
 
 
             Console.ReadKey();
