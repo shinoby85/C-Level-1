59b7b336eee659535f3badd89b477a50a685d34a
shinoby85 <ponk@tut.by>
2019-02-15 00:09:30 +0300

    Добавлен пункт в плюс отладка

--- Lesson 7/Udvoitel.Designer.cs
+++ Lesson 7/Udvoitel.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnCommand1 = new System.Windows.Forms.Button();
             this.btnCommand2 = new System.Windows.Forms.Button();
-            this.btnReset = new System.Windows.Forms.Button();
+            this.btnBackUP = new System.Windows.Forms.Button();
             this.lblNumber = new System.Windows.Forms.Label();
             this.lblSumm = new System.Windows.Forms.Label();
             this.gbStatus = new System.Windows.Forms.GroupBox();
@@ -39,6 +39,7 @@
             this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnReset = new System.Windows.Forms.ToolStripMenuItem();
             this.gbStatus.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -63,15 +64,15 @@
             this.btnCommand2.UseVisualStyleBackColor = true;
             this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
             // 
-            // btnReset
+            // btnBackUP
             // 
-            this.btnReset.Location = new System.Drawing.Point(271, 214);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(136, 41);
-            this.btnReset.TabIndex = 0;
-            this.btnReset.Text = "Сброс";
-            this.btnReset.UseVisualStyleBackColor = true;
-            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            this.btnBackUP.Location = new System.Drawing.Point(271, 214);
+            this.btnBackUP.Name = "btnBackUP";
+            this.btnBackUP.Size = new System.Drawing.Size(136, 41);
+            this.btnBackUP.TabIndex = 0;
+            this.btnBackUP.Text = "Отмена";
+            this.btnBackUP.UseVisualStyleBackColor = true;
+            this.btnBackUP.Click += new System.EventHandler(this.btnBackUP_Click);
             // 
             // lblNumber
             // 
@@ -128,6 +129,7 @@
             // 
             this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.игратьToolStripMenuItem,
+            this.btnReset,
             this.выходToolStripMenuItem});
             this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
             this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
@@ -147,6 +149,13 @@
             this.выходToolStripMenuItem.Text = "Выход";
             this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
             // 
+            // btnReset
+            // 
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(180, 22);
+            this.btnReset.Text = "Сброс";
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
             // formUdvoitel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -154,7 +163,7 @@
             this.ClientSize = new System.Drawing.Size(438, 352);
             this.Controls.Add(this.gbStatus);
             this.Controls.Add(this.lblNumber);
-            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnBackUP);
             this.Controls.Add(this.btnCommand2);
             this.Controls.Add(this.btnCommand1);
             this.Controls.Add(this.menuStrip1);
@@ -175,7 +184,7 @@
 
         private System.Windows.Forms.Button btnCommand1;
         private System.Windows.Forms.Button btnCommand2;
-        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnBackUP;
         private System.Windows.Forms.Label lblNumber;
         private System.Windows.Forms.Label lblSumm;
         private System.Windows.Forms.GroupBox gbStatus;
@@ -184,5 +193,6 @@
         private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnReset;
     }
 }
--- Lesson 7/Udvoitel.cs
+++ Lesson 7/Udvoitel.cs
@@ -13,39 +13,62 @@ namespace Lesson_7
     public partial class formUdvoitel : Form
     {
         private static int schet;
-        
+        private static bool flag;
+        private Stack<int> myStack;
+
+
         public formUdvoitel()
         {
             schet = 0;
-            
+            flag = false;
             InitializeComponent();
             gbStatus.Visible = false;
             btnCommand1.Enabled = false;
             btnCommand2.Enabled = false;
-            btnReset.Enabled = false;
+            btnBackUP.Enabled = false;
+            myStack = new Stack<int>();
         }
 
         private void btnCommand1_Click(object sender, EventArgs e)
         {
+            
+            flag = true;
             lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
             gbStatus.Visible = true;
             lblSumm.Text = (int.Parse(lblSumm.Text) + 1).ToString();
+            myStack.Push(int.Parse(lblNumber.Text));
             Analiz();
+            if (lblSumm.Text != "0") btnBackUP.Enabled = true;
         }
 
         private void btnCommand2_Click(object sender, EventArgs e)
         {
+            
+            flag = true;
             lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
             gbStatus.Visible = true;
             lblSumm.Text = (int.Parse(lblSumm.Text) + 1).ToString();
+            myStack.Push(int.Parse(lblNumber.Text));
             Analiz();
+            if (lblSumm.Text != "0") btnBackUP.Enabled = true;
         }
 
-        private void btnReset_Click(object sender, EventArgs e)
+        private void btnBackUP_Click(object sender, EventArgs e)
         {
-            lblNumber.Text = "0";
-            gbStatus.Visible = false; ;
-            lblSumm.Text = "0";
+            if (flag==true)
+            {
+                flag = false;
+                lblNumber.Text = myStack.Pop().ToString();
+
+            }
+            if (myStack.Count != 0)
+            {
+                lblNumber.Text = myStack.Pop().ToString();
+            }
+            else lblNumber.Text = "0";
+
+            lblSumm.Text = (int.Parse(lblSumm.Text) - 1).ToString();
+            if (lblSumm.Text == "0") btnBackUP.Enabled = false;
         }
 
         private void formUdvoitel_Load(object sender, EventArgs e)
@@ -64,7 +87,7 @@ namespace Lesson_7
                 "Условия игры");
             btnCommand1.Enabled = true;
             btnCommand2.Enabled = true;
-            btnReset.Enabled = true;
+            btnBackUP.Enabled = true;
         }
         /// <summary>
         /// Проверка на совпадение с требуемым числом
@@ -75,7 +98,7 @@ namespace Lesson_7
             {
                 btnCommand1.Enabled = false;
                 btnCommand2.Enabled = false;
-                btnReset.Enabled = false;
+                btnBackUP.Enabled = false;
                 MessageBox.Show($"Поздравляем вы выйграли!!!\n" +
                     $"Для достижения своей цели вы совершили {lblSumm.Text} хода(-ов)." +
                     $"","Информация",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
@@ -84,7 +107,7 @@ namespace Lesson_7
             {
                 btnCommand1.Enabled = false;
                 btnCommand2.Enabled = false;
-                btnReset.Enabled = false;
+                btnBackUP.Enabled = false;
                 MessageBox.Show($"Ваше значение превышае требуемое число!!!\n" +
                     $"Для вас игра закончилась на {lblSumm.Text} ходу.","Информация",MessageBoxButtons.OK,MessageBoxIcon.Stop);
             }            
@@ -94,6 +117,13 @@ namespace Lesson_7
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            lblNumber.Text = "0";
+            gbStatus.Visible = false; ;
+            lblSumm.Text = "0";
         }
     }
 }
