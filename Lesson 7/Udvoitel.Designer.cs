cbd83e77912bdafe9a49bbc93df41b618e6c71be
shinoby85 <ponk@tut.by>
2019-02-14 23:26:53 +0300

    Выполнение пунктов а и б задания 1

--- Lesson 7/Udvoitel.Designer.cs
+++ Lesson 7/Udvoitel.Designer.cs
@@ -32,6 +32,15 @@
             this.btnCommand2 = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.lblNumber = new System.Windows.Forms.Label();
+            this.lblSumm = new System.Windows.Forms.Label();
+            this.gbStatus = new System.Windows.Forms.GroupBox();
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gbStatus.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCommand1
@@ -73,17 +82,90 @@
             this.lblNumber.TabIndex = 1;
             this.lblNumber.Text = "0";
             // 
+            // lblSumm
+            // 
+            this.lblSumm.AutoSize = true;
+            this.lblSumm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSumm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSumm.Location = new System.Drawing.Point(334, 16);
+            this.lblSumm.Name = "lblSumm";
+            this.lblSumm.Size = new System.Drawing.Size(22, 26);
+            this.lblSumm.TabIndex = 2;
+            this.lblSumm.Text = "0";
+            // 
+            // gbStatus
+            // 
+            this.gbStatus.Controls.Add(this.lblText1);
+            this.gbStatus.Controls.Add(this.lblSumm);
+            this.gbStatus.Location = new System.Drawing.Point(26, 277);
+            this.gbStatus.Name = "gbStatus";
+            this.gbStatus.Size = new System.Drawing.Size(381, 63);
+            this.gbStatus.TabIndex = 3;
+            this.gbStatus.TabStop = false;
+            this.gbStatus.Text = "Статистика";
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblText1.Location = new System.Drawing.Point(23, 22);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(286, 20);
+            this.lblText1.TabIndex = 3;
+            this.lblText1.Text = "Количество совершенных операций:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(438, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            this.игратьToolStripMenuItem.Click += new System.EventHandler(this.игратьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
             // formUdvoitel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 298);
+            this.ClientSize = new System.Drawing.Size(438, 352);
+            this.Controls.Add(this.gbStatus);
             this.Controls.Add(this.lblNumber);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCommand2);
             this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "formUdvoitel";
             this.Text = "Удвоитель";
+            this.Load += new System.EventHandler(this.formUdvoitel_Load);
+            this.gbStatus.ResumeLayout(false);
+            this.gbStatus.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,5 +177,12 @@
         private System.Windows.Forms.Button btnCommand2;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblSumm;
+        private System.Windows.Forms.GroupBox gbStatus;
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
     }
 }
--- Lesson 7/Udvoitel.cs
+++ Lesson 7/Udvoitel.cs
@@ -12,25 +12,88 @@ namespace Lesson_7
 {
     public partial class formUdvoitel : Form
     {
+        private static int schet;
+        
         public formUdvoitel()
         {
+            schet = 0;
+            
             InitializeComponent();
+            gbStatus.Visible = false;
+            btnCommand1.Enabled = false;
+            btnCommand2.Enabled = false;
+            btnReset.Enabled = false;
         }
 
         private void btnCommand1_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            gbStatus.Visible = true;
+            lblSumm.Text = (int.Parse(lblSumm.Text) + 1).ToString();
+            Analiz();
         }
 
         private void btnCommand2_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
-
+            gbStatus.Visible = true;
+            lblSumm.Text = (int.Parse(lblSumm.Text) + 1).ToString();
+            Analiz();
         }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
-            lblNumber.Text = "1";
+            lblNumber.Text = "0";
+            gbStatus.Visible = false; ;
+            lblSumm.Text = "0";
+        }
+
+        private void formUdvoitel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void игратьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "0";
+            gbStatus.Visible = false;
+            lblSumm.Text = "0";
+            Random rnd = new Random();
+            schet=rnd.Next(1, 100);
+            MessageBox.Show($"Для победы вам нужно набрать число: {schet}","" +
+                "Условия игры");
+            btnCommand1.Enabled = true;
+            btnCommand2.Enabled = true;
+            btnReset.Enabled = true;
+        }
+        /// <summary>
+        /// Проверка на совпадение с требуемым числом
+        /// </summary>        
+        private void Analiz()
+        {
+            if (int.Parse(lblNumber.Text)==schet)
+            {
+                btnCommand1.Enabled = false;
+                btnCommand2.Enabled = false;
+                btnReset.Enabled = false;
+                MessageBox.Show($"Поздравляем вы выйграли!!!\n" +
+                    $"Для достижения своей цели вы совершили {lblSumm.Text} хода(-ов)." +
+                    $"","Информация",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+            }
+            else if (int.Parse(lblNumber.Text) > schet)
+            {
+                btnCommand1.Enabled = false;
+                btnCommand2.Enabled = false;
+                btnReset.Enabled = false;
+                MessageBox.Show($"Ваше значение превышае требуемое число!!!\n" +
+                    $"Для вас игра закончилась на {lblSumm.Text} ходу.","Информация",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+            }            
+            
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
