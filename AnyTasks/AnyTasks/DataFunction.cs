28d901d12b0b2ed367cb9499eecff324a2c3222a
shinoby85 <ponk@tut.by>
2019-03-04 18:57:03 +0300

    Создан класс для хранения кода по заданиям с различных уроков.

---
+++ AnyTasks/AnyTasks/DataFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyTasks
+{
+    class DataFunction
+    {
+        
+        public void Task_4_1()
+        {
+
+
+
+        }
+      
+
+
+
+
+
+
+    }
+}
