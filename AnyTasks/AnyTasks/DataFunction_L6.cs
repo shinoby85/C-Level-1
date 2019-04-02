80435689bd48fc861e205377485d208101f915dc
shinoby85 <ponk@tut.by>
2019-04-02 18:32:13 +0300

    Добавление данных для выполнения заданий 3 и 4

--- AnyTasks/AnyTasks/DataFunction_L6.cs
+++ AnyTasks/AnyTasks/DataFunction_L6.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
 
 namespace AnyTasks
 {
     class DataFunction_L6
     {
+        public void Task_6_2() {
+
+            int bakalavr = 0;
+            int magistr = 0;
+            // Создадим необобщенный список
+            ArrayList list = new ArrayList();
+            // Запомним время в начале обработки данных
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("..\\..\\students_1.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    // Console.WriteLine("{0}", s[0], s[1], s[2], s[3], s[4]);
+                    list.Add(s[1] + " " + s[0]);// Добавляем склееные имя и фамилию
+                    if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+                }
+                catch
+                {
+                }
+            }
+            sr.Close();
+            list.Sort();
+            Console.WriteLine("Всего студентов:{0}", list.Count);
+            Console.WriteLine("Магистров:{0}", magistr);
+            Console.WriteLine("Бакалавров:{0}", bakalavr);
+            foreach (var v in list) Console.WriteLine(v);
+            // Вычислим время обработки данных
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+
+
+
+        }
+
     }
 }
